2011006a9f8121ace5a0bdf3ccbe5c31e960d6ee
James Mateo <mr.jamesmateo22@gmail.com>
2026-04-03 02:59:48 +0800

    Initial commit - folder dir

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="?">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <link rel="icon" href="?" type="image/png">
+        <link rel="stylesheet" href="?">
+
+        <title>?</title>
+    </head>
+
+    <body>
+        <?php
+            require_once __DIR__ . '/../app/try.php';    
+       ?>
+    </body>
+
+</html>
