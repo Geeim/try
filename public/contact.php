0bc96de9b30fe846222b31368643d2b74eb73794
James Mateo <mr.jamesmateo22@gmail.com>
2026-04-03 20:13:59 +0800

    Delete Contact

--- public/contact.php
+++
@@ -1,8 +0,0 @@
-<h2>Contact Page</h2>
-<p>Para sa karagdagang impormasyon, maari mo kaming kontakin sa pamamagitan ng:</p>
-<ul>
-    <li>Email: <a href="mailto:mr.jamesmateo22@gmail.com">mr.jamesmateo22@gmail.com</a></li>
-    <li>Facebook: <a href="https://www.facebook.com/kosa" target="_blank">Kosa FB</a></li>
-    <li>Mobile: +63 912 345 6789</li>
-</ul>
-<p>Puwede mo rin gamitin ang form sa susunod na update para direktang magpadala ng mensahe.</p>
