c18649ff8e54ae21f0c8e032e5f76b2eb3bb4caa
James Mateo <mr.jamesmateo22@gmail.com>
2026-04-03 19:47:09 +0800

    Testing Debug while running Docker

--- public/about.php
+++
@@ -1,8 +0,0 @@
-<h2>About Page</h2>
-<p>Ang project na ito ay ginawa ni Kosa 😎 gamit ang PHP at Docker.</p>
-<p>Layunin nitong ipakita ang clean architecture at modular approach gamit ang front controller pattern.</p>
-<ul>
-    <li>Ang index.php ang front controller na nagha-handle ng lahat ng requests</li>
-    <li>Ang bawat page ay hiwalay na PHP file para sa content</li>
-    <li>Madali i-maintain at scalable</li>
-</ul>
